f8650a3f8839d2b445eafda2b836df66e9ea0a86
Tony Sims <zocle@zocle.net>
2021-06-08 18:23:43 +0000

    fixed typo'd nbFiles

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -9,7 +9,7 @@ class AppController {
 
   static async getStats(request, response) {
     const numUsers = await db.nbUsers();
-    const numFiles = await db.nbfiles();
+    const numFiles = await db.nbFiles();
     const jsonObj = { users: numUsers, files: numFiles };
     return response.status(200).send(jsonObj);
   }
